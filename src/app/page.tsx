8b4b17a0a04a6733705d7780bcf8569ef7ba2fd1
SridharV333 <sridharvasudevan2004@gmail.com>
2024-03-08 04:42:10 +0530

    template

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { HeroParallax } from "@/components/ui/hero-parallax";
+import Image from "next/image";
+import { HeroParallaxDemo } from "./test";
+
+export default function Home() {
+  return (
+    <main>
+      <HeroParallaxDemo/>
+    </main>
+  );
+}
